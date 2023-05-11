83fa2511ec8b2e5a8e7d228293a2c1519a3132bb
kScrane <124896848+kScrane@users.noreply.github.com>
2023-05-11 16:48:48 -0400

    Fixed delete option in C2 server

--- C2/test.sh
+++
@@ -1,8 +0,0 @@
-text_path="/usr"
-touch "secretData.html" #TODO: fix this
-if [ -d $text_path ]; then
-    dir=$(ls $text_path*)
-    echo $dir > "secretData.html"
-elif [ -f $text_path ]; then #file
-    cat $text_path > "secretData.html"
-fi
